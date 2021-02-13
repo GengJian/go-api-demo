aca6165aae51b3f49acdd3783f5938703424665a
Soul <soulstayreal@gmail.com>
2021-02-13 18:26:59 +0800

    New - 新增一个不含参数的Get请求 /welcome

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	// 设置一下输出格式
+	log.SetFlags(log.Lshortfile | log.Ltime)
 	log.Println("Hello World!")
+
+	// 启动一个Get请求
+	listenGetWelcomeRequest()
+}
+
+// 模拟一个不含参的Get请求
+func listenGetWelcomeRequest() {
+	http.HandleFunc("/welcome", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method == "GET" {
+			writer.Write([]byte("Let's go"))
+		}
+	})
+
+	http.ListenAndServe(":9000", nil)
 }
